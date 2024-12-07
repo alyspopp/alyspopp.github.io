ec256e8125c375abee361c8b3a3732dc219d2bd3
alyspopp <150248345+alyspopp@users.noreply.github.com>
2024-12-07 22:18:13 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+$('.block1-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: '<svg class="slidernext" width="55.582825" height="55.582764" viewBox="0 0 55.5828 55.5828" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs/><circle id="Ellipse 1979" cx="27.791443" cy="27.791504" r="27.791418" fill="#FFFFFF" fill-opacity="1.000000"/><path id="angle-right" d="M34.44 27.11L26.53 18.7C26.03 18.14 25.2 18.14 24.65 18.64C24.09 19.14 24.09 19.97 24.59 20.52L31.68 28L24.59 35.52C24.09 36.08 24.09 36.91 24.65 37.4C25.2 37.9 26.03 37.9 26.53 37.35L34.44 28.94C34.67 28.66 34.83 28.33 34.83 28C34.83 27.72 34.67 27.39 34.44 27.11Z" fill="#E21F4D" fill-opacity="1.000000" fill-rule="evenodd"/></svg>',
+        prevArrow: '<div class="sliderprev"></div>'
+    });
+
+    $('.uslugi-block_slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true, 
+        centerPadding: '25px',
+        nextArrow: '<svg class="slidernext2" width="55.582825" height="55.582764" viewBox="0 0 55.5828 55.5828" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs/><circle id="Ellipse 1979" cx="27.791443" cy="27.791504" r="27.791418" fill="#FFFFFF" fill-opacity="1.000000"/><path id="angle-right" d="M34.44 27.11L26.53 18.7C26.03 18.14 25.2 18.14 24.65 18.64C24.09 19.14 24.09 19.97 24.59 20.52L31.68 28L24.59 35.52C24.09 36.08 24.09 36.91 24.65 37.4C25.2 37.9 26.03 37.9 26.53 37.35L34.44 28.94C34.67 28.66 34.83 28.33 34.83 28C34.83 27.72 34.67 27.39 34.44 27.11Z" fill="#E21F4D" fill-opacity="1.000000" fill-rule="evenodd"/></svg>',
+        prevArrow: '<svg class="sliderprev2" width="55.000000" height="55.000000" viewBox="0 0 55 55" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs/><circle id="Ellipse 1980" r="27.500000" transform="matrix(-1 0 0 1 27.5 27.5)" fill="#FFFFFF" fill-opacity="1.000000"/><path id="angle-right" d="M22.16 27.11L30.07 18.7C30.57 18.14 31.4 18.14 31.95 18.64C32.5 19.14 32.5 19.97 32.01 20.52L24.92 28L32.01 35.52C32.5 36.08 32.5 36.91 31.95 37.4C31.4 37.9 30.57 37.9 30.07 37.35L22.16 28.94C21.93 28.66 21.77 28.33 21.77 28C21.77 27.72 21.93 27.39 22.16 27.11Z" fill="#E21F4D" fill-opacity="1.000000" fill-rule="evenodd"/></svg>'
+    });
+
+
+
+ });
